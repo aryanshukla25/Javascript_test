504763b7619197779cc276027a6cda5655f8a9cb
Aryan Shukla <aryanshukla25@gmail.com>
2024-06-25 22:15:21 +0530

    Async and Sync

--- class 1/main.js
+++ class 1/main.js
@@ -1,6 +1,18 @@
 sum = 0;
-for (let i=0;i<10;i++){
+for (let i=0;i<10 ;i++){
     sum = sum + i;
 
 }
 console.log(sum)
+
+
+//Async and Sync functions
+
+// 1. setTimeout(() => {
+    
+// }, timeout);
+
+// 2.fs.readFile
+
+// 3. Fetch
+
